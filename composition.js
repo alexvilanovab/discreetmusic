acd79a7bbdd03882e07bda813cc7775624415dcb
Natalia Galán <hi@nataliagalan.me>
2025-04-08 11:43:58 -0700

    implementing envelope and filterEnvelope

--- composition.js
+++ composition.js
@@ -1,31 +1,54 @@
 function playNote() {
-  let envelope = new Tone.Envelope({
-    attack: 0.1,
-    release: 4,
-  }).toDestination();
-  envelope.releaseCurve = 'linear';
-  
-  let filterEnvelope = {
-    baseFrequency: 200,
-    octaves: 2,
-    attack: 0,
-    decay: 0,
-    release: 1000
-  };
-  let synth = new Tone.DuoSynth({
-    harmonicity: 1,
-    voice0: {
-      oscillator: {type: 'sawtooth'},
-      envelope,
-      filterEnvelope
+  const synth = new Tone.MonoSynth({
+    oscillator: {
+      type: "sawtooth",
     },
-    voice1: {
-      oscillator: {type: 'sine'},
-      envelope,
-      filterEnvelope
+    envelope: {
+      attack: 0.3,
+      attackCurve: "linear",
+      release: 4,
+      releaseCurve: "linear",
     },
-    vibratoRate: 0.5,
-    vibratoAmount: 0.1
+    filterEnvelope: {
+      attack: 0,
+      decay: 0,
+      release: 1000,
+      baseFrequency: 100,
+      octaves: 2,
+    },
   }).toDestination();
-  synth.triggerAttackRelease('C4', 1);
+
+  synth.triggerAttackRelease("C4", 1);
 }
+
+// function playNote() {
+//   let envelope = new Tone.Envelope({
+//     attack: 0.1,
+//     release: 4,
+//   }).toDestination();
+//   envelope.releaseCurve = "linear";
+
+//   let filterEnvelope = {
+//     baseFrequency: 200,
+//     octaves: 2,
+//     attack: 0,
+//     decay: 0,
+//     release: 1000,
+//   };
+//   let synth = new Tone.DuoSynth({
+//     harmonicity: 1,
+//     voice0: {
+//       oscillator: { type: "sawtooth" },
+//       envelope,
+//       filterEnvelope,
+//     },
+//     voice1: {
+//       oscillator: { type: "sine" },
+//       envelope,
+//       filterEnvelope,
+//     },
+//     vibratoRate: 0.5,
+//     vibratoAmount: 0.1,
+//   }).toDestination();
+//   synth.triggerAttackRelease("C4", 1);
+// }
